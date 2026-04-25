8dcc1dff7b916191bbfc0aabd9af9c4221ceb297
Martin Evans <martindevans@gmail.com>
2026-04-25 01:38:56 +0100

    Fixed Morpeh entity creation  - Create3 benchmark created entity with 2 components  - Create2 benchmark created entity with 3 components  - Both benchmarks actually created N separate entities, instead of one entity with N components

--- source/Ecs.CSharp.Benchmark/CreateEntityWithTwoComponents/Morpeh.cs
+++ source/Ecs.CSharp.Benchmark/CreateEntityWithTwoComponents/Morpeh.cs
@@ -16,9 +16,9 @@ namespace Ecs.CSharp.Benchmark
             World world = _context.World;
             for (int i = 0; i < EntityCount; ++i)
             {
-                world.CreateEntity().AddComponent<MorpehBaseContext.Component1>();
-                world.CreateEntity().AddComponent<MorpehBaseContext.Component2>();
-                world.CreateEntity().AddComponent<MorpehBaseContext.Component3>();
+                var entity = world.CreateEntity();
+                entity.AddComponent<MorpehBaseContext.Component1>();
+                entity.AddComponent<MorpehBaseContext.Component2>();
             }
 
             world.Commit();
@@ -31,13 +31,11 @@ namespace Ecs.CSharp.Benchmark
             World world = _context.World;
             Stash<MorpehBaseContext.Component1> stash1 = world.GetStash<MorpehBaseContext.Component1>();
             Stash<MorpehBaseContext.Component2> stash2 = world.GetStash<MorpehBaseContext.Component2>();
-            Stash<MorpehBaseContext.Component3> stash3 = world.GetStash<MorpehBaseContext.Component3>();
             for (int i = 0; i < EntityCount; ++i)
             {
                 Entity entity = world.CreateEntity();
                 stash1.Add(entity);
                 stash2.Add(entity);
-                stash3.Add(entity);
             }
 
             world.Commit();
